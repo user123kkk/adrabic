ad72beba6bac1111c6e21fe933e756474df3eca1
user123kkk <masjeedikk@gmail.com>
2026-09-11 16:31:54 +0200

    Update CACHE_NAME to version 2.21.4

--- wiederholung/sw.js
+++ wiederholung/sw.js
@@ -7,7 +7,7 @@
    WICHTIG: Bei jeder neuen Version CACHE_NAME hochzählen (v2 → v3 → ...),
    sonst behalten Nutzer:innen alte Dateien im Cache. */
 
-const CACHE_NAME = "adrabic-2.20.0";
+const CACHE_NAME = "adrabic-2.21.4";
 
 const APP_SHELL = [
   "./",
